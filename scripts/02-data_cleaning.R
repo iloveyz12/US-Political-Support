5c72e189bc89099e571d54052b8cae05c68f4e6b
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-03-11 23:21:36 -0400

    Data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,11 +1,8 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the raw plane data 
+# Authors: Zheng(Zeb) Yang, Dongli Sun, Bernice(Yixuan) Bao
+# Contact: zhengzeb.yang@mail.utoronto.ca, dongli.sun@mail.utoronto.ca, bernice.bao@mail.utoronto.ca
+# Pre-requisites: -
 
 #### Workspace setup ####
 library(tidyverse)
