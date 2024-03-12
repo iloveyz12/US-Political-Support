3fde7b1447474a0ad3575c205f926b4fdb376622
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-03-11 23:22:11 -0400

    Data cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,29 +9,33 @@ library(tidyverse)
 library(arrow)
 
 #### Clean data ####
-ces2020 <-
+ces2022 <-
   read_csv(
-    here::here("data/raw_data/ces2020.csv"),
+    here::here("data/raw_data/ces2022.csv"),
     col_types =
       cols(
         "votereg" = col_integer(),
-        "CC20_410" = col_integer(),
-        "gender" = col_integer(),
+        "presvote20post" = col_integer(),
+        "gender4" = col_integer(),
         "educ" = col_integer()
       )
   )
 
-ces2020
+ces2022
 
 
-ces2020 <-
-  ces2020 |>
+ces2022 <-
+  ces2022 |>
   filter(votereg == 1,
-         CC20_410 %in% c(1, 2)) |>
+         presvote20post %in% c(1, 2)) |>
   mutate(
-    voted_for = if_else(CC20_410 == 1, "Biden", "Trump"),
+    voted_for = if_else(presvote20post == 1, "Biden", "Trump"),
     voted_for = as_factor(voted_for),
-    gender = if_else(gender == 1, "Male", "Female"),
+    gender = case_when(
+      gender4 == 1 ~ "Man", 
+      gender4 == 2 ~ "Woman", 
+      gender4 == 3 ~ "Non-binary", 
+      gender4 == 4 ~ "Other"),
     education = case_when(
       educ == 1 ~ "No HS",
       educ == 2 ~ "High school graduate",
@@ -56,4 +60,4 @@ ces2020 <-
 
 
 #### Save data ####
-write_parquet(ces2020, "data/analysis_data/analysis_data.parquet")
+write_parquet(ces2022, "data/analysis_data/analysis_data.parquet")
