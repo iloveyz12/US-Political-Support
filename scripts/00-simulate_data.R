61e1def69f6c0bb4646ba75774e554d6754f6abf
Zheng(Zeb) Yang <zhengzeb.yang@mail.utoronto.ca>
2024-03-11 23:12:45 -0400

    Simulate the data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,39 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates data on 
+# Authors: Zheng(Zeb) Yang, Dongli Sun, Bernice(Yixuan) Bao
+# Contact: zhengzeb.yang@mail.utoronto.ca, dongli.sun@mail.utoronto.ca, bernice.bao@mail.utoronto.ca
+# Pre-requisites: -
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(812)
 
+num_obs <- 1000
 
+us_political_preferences <- tibble(
+  education = sample(0:4, size = num_obs, replace = TRUE),
+  gender = sample(1:4, size = num_obs, replace = TRUE),
+  support_prob = ((education + gender) / 5),
+) |>
+  mutate(
+    supports_biden = if_else(runif(n = num_obs) < support_prob, "yes", "no"),
+    education = case_when(
+      education == 0 ~ "< High school",
+      education == 1 ~ "High school",
+      education == 2 ~ "Some college",
+      education == 3 ~ "College",
+      education == 4 ~ "Post-grad"
+    ),
+    gender = case_when(
+      gender == 1 ~ "Man", 
+      gender == 2 ~ "Woman", 
+      gender == 3 ~ "Non-binary", 
+      gender == 4 ~ "Other")
+  ) |>
+  select(-support_prob, supports_biden, gender, education)
+
+write.csv(us_political_preferences, "data/simulated_data/simulated_data.csv")
 
